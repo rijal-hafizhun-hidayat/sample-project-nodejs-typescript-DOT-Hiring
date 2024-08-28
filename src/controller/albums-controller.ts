8435a3778c0cfed4a71913a4f5f065134e5c1c8a
unknown <rijal.1344@gmail.com>
2024-08-28 15:34:29 +0700

    feat: add albums

---
+++ src/controller/albums-controller.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from "express";
+import { AlbumsService } from "../service/albums-service";
+import { AlbumsRequest } from "../model/album-model";
+
+export class AlbumsController {
+  static async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await AlbumsService.getAll();
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async store(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request: AlbumsRequest = req.body as AlbumsRequest;
+      const result = await AlbumsService.store(request);
+
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async findByAlbumId(req: Request, res: Response, next: NextFunction) {
+    try {
+      const albumId: number = parseInt(req.params.albumId);
+      const result = await AlbumsService.findByAlbumId(albumId);
+
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async updateByAlbumId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const albumId: number = parseInt(req.params.albumId);
+      const request: AlbumsRequest = req.body as AlbumsRequest;
+      const result = await AlbumsService.updateByAlbumId(albumId, request);
+
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
